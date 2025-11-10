d90b44f4293ed7f6bae95f4df1b3b095b1e2af5a
Karimsuhail <karimsuhaildev@gmail.com>
2025-11-10 22:49:29 +0530

    case mismatch fixed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -35,11 +35,11 @@ app.use("/api/messages", messageRoutes);
 
 // ✅ Serve frontend build in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
-  });
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
 }
 
 // ✅ Server start
